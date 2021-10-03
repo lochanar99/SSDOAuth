bd24b3b77147269ad0d2c41525be5a7172859297
Pramodh Rajapakse <pramodh.rajapakse@nichelabs.lk>
2021-10-03 12:03:38 +0530

    added get request to index file

--- service/index.js
+++ service/index.js
@@ -24,3 +24,34 @@ app.listen(port, () => {            //server starts listening for any attempts f
 //set view engine to ejs
 app.set("view engine", "ejs");
 
+
+app.get('/', (req, res) => {        //get requests to the root ("/") will route here
+    // authorization uri
+    const authUrl = "https://accounts.google.com/o/oauth2/v2/auth?";
+
+    // access_type
+    const access_type = "access_type=offline&";
+    // scopes
+    const scope =
+        "scope=" +
+        encodeURIComponent(
+            "https://www.googleapis.com/auth/drive.file" +
+                " " +
+                "https://www.googleapis.com/auth/drive.metadata.readonly" +
+                " " +
+                "https://www.googleapis.com/auth/userinfo.profile"
+        ) +
+        "&";
+    // response type
+    const response_type = "response_type=code&";
+    // client id
+    const client_id = "client_id=" + CLIENT_ID + "&";
+    // redirect uri
+    const redirect_uri = "redirect_uri=" + REDIRECT_URL;
+
+    //#endregion
+
+    // prepare url and return
+    const oauthUrl = authUrl + access_type + scope + response_type + client_id + redirect_uri;
+    res.render("index", { url: oauthUrl });
+});
